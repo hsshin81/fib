090c04653097af41cde9aab75a89f9daf605780e
Heesung Shin <heesung.shin@fdmgroup.com>
2023-04-05 22:11:30 -0400

    first commit

---
+++ fib.py
@@ -0,0 +1,11 @@
+fibnum = int(input("Enter number of fib values: "))
+fiblist = []
+
+for i in range(fibnum):
+    if i < 2:
+        fiblist.append(i)
+    else:
+        fiblist.append((fiblist[i-1]+fiblist[i-2]))
+
+print(fiblist)
+
